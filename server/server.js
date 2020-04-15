dbd99ec07b5f84ece59e5de5a4f4a265caec36cf
faruqneo <faruq_khan@yahoo.com>
2020-04-15 12:24:03 +0530

    port

--- server/server.js
+++ server/server.js
@@ -81,7 +81,7 @@ app.get('/', async (req, res) => {
 });
 
 
-app.listen(PORT, () => {
+app.listen(port, () => {
     mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(res => console.log(`server is running on port ${port}\nConnected with db.`))
         .catch(err => console.log('Unable to connected with db.'))
